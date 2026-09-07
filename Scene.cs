ee8a2ecebfbef676442b918cb10fa1ec180c292e
RDeBont <rowandebont@outlook.com>
2026-09-07 12:06:52 +0200

    scene-model toevoegen
    Scene.cs — Id, Beschrijving, Hint, Akt, Keuzes, ItemBijKeuze.

---
+++ Scene.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Text_Based_adventure
+{
+    internal class Scene
+    {
+        public string Id { get; set; } = "";
+        public string Beschrijving { get; set; } = "";
+        public string Hint { get; set; } = "";
+        public int Akt { get; set; } = 1;
+        public Dictionary<string, string> Keuzes { get; set; } = new();
+        public Dictionary<string, string> ItemBijKeuze { get; set; } = new();
+    }
+}
