7098ded121a2186a9ceb61976341b2759b2c7170
RDeBont <rowandebont@outlook.com>
2026-09-07 12:07:43 +0200

    spelstatus-model toevoegen

---
+++ SpelStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Text_Based_adventure
+{
+    internal class SpelStatus
+    {
+        public string HuidigeScene { get; set; } = "ingang";
+        public List<string> Tas { get; set; } = new();
+    }
+}
